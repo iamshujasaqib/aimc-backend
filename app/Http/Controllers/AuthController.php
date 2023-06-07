0c9f5f6bd62c91f607110e57aa4cf21480eeb404
iamshujasaqib <iamshujasaqib@gmail.com>
2023-06-07 06:38:18 +0500

    validating user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
-    //
+    public function login( Request $request ){
+        $data = $request->validate([
+            'email'         => ['required', 'email'],
+            'password'      => 'required',
+        ]);
+
+        $user = User::whereEmail($data['email'])->first();
+        if(!$user){
+            abort(400, "This user does not exist");
+        }
+
+        if (Auth::attempt($data)) {
+            return new UserResource($user);
+        }
+
+        return ['message' => 'Invalid credentials'];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/users/register', [UsersController::class,'register']);
+Route::post('/auth/login', [AuthController::class,'login']);
